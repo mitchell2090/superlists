4d6fef2a8f126197c714c01f004d6d613d11d183
Bill Mitchell <mitchell2090@bellsouth.net>
2014-09-10 11:50:30 -0400

    Added CSS code via bootstrap

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,11 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 # import unittest
 
 
 
-class NewVisiterTest (LiveServerTestCase) :
+class NewVisiterTest (StaticLiveServerTestCase) :
     def setUp(self) :
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
