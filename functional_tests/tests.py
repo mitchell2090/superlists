f6d2b3c2b0bf59a3f5915bda8b4f0e6d8c4101ad
Bill Mitchell <mitchell2090@bellsouth.net>
2014-09-11 16:03:01 -0400

    Hack FT to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,10 +2,26 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 # import unittest
-
+import sys
 
 
 class NewVisiterTest (StaticLiveServerTestCase) :
+    
+    @classmethod
+    def setUpClass(cls) :
+        print(sys.argv)
+        for arg in sys.argv:
+            if 'liveserver' in arg :
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls) :
+        if cls.server_url == cls.live_server_url :
+            super().tearDownClass()
+    
     def setUp(self) :
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -22,7 +38,8 @@ class NewVisiterTest (StaticLiveServerTestCase) :
     def test_can_start_a_list_and_retrieve_it_later(self) :
         # Edith has heard about a to-do app.  She is obcessive: she has
         # twenty-seven already, but can't let one go by without trying it. 
-        self.browser.get(self.live_server_url)
+        print("\n\nself.browser.get(" + self.server_url + ")\n\n")
+        self.browser.get(self.server_url)
         # She notices that the page title and header mention to-do lists.
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -68,7 +85,7 @@ class NewVisiterTest (StaticLiveServerTestCase) :
         self.browser = webdriver.Firefox()
 
         # Francis visits the site.  There is not sign of Edith's list.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('peacock', page_text)
         self.assertNotIn('kill a fly', page_text)
@@ -90,7 +107,7 @@ class NewVisiterTest (StaticLiveServerTestCase) :
 
     def test_layout_and_styling(self) :
         # Edith goes to the home page.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # the input box is centered.  Bully
