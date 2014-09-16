aae8406753f5d0784ea4865ad8d95550937bd6cc
Bill Mitchell <mitchell2090@bellsouth.net>
2014-09-15 22:50:13 -0400

    Refactor list view to handle new item POSTS.

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -85,14 +85,12 @@ class NewListTest(TestCase) :
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
-class NewItemTest (TestCase) :
-
     def test_can_save_a_POST_request_to_an_existing_list(self):
         lists = { x : List.objects.create() for x in ['correct_', 'other_']}
         test_text = "New item for existing list"
 
         self.client.post(
-            '/lists/{:d}/add_item'.format(lists['correct_'].id),
+            '/lists/{:d}/'.format(lists['correct_'].id),
             data = {'item_text': test_text}
         )
 
@@ -103,10 +101,10 @@ class NewItemTest (TestCase) :
         self.assertEqual(new_item.list, lists['correct_'])
         
 
-    def test_redirects_to_list_view(self) :
+    def test_POST_redirects_to_list_view(self) :
         test_text = 'test redirect to list'
-        lists = { x : List.objects.create() for x in ['correct_', 'other_']}
-        list_url = '/lists/{:d}/'.format(lists['correct_'].id)
-        post_url = list_url + 'add_item'
+        lists = { x : List.objects.create() for x in ['correct', 'other']}
+        list_url = '/lists/{:d}/'.format(lists['correct'].id)
+        post_url = list_url
         response = self.client.post( post_url, data = {'item_text' : test_text} )
         self.assertRedirects(response, list_url)
--- lists/views.py
+++ lists/views.py
@@ -14,8 +14,10 @@ def home_page(request) :
     return(render(request, 'home.html'))
 
 def view_list(request, list_id) :
-#    entering_info('view_list', args=[request, list_id])
     list_= List.objects.get(id=list_id)
+    if request.method == 'POST' :
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/{:d}/'.format(list_.id))
     return render(request, 'list.html', {'list' : list_ })
 
 def new_list(request) :
@@ -29,9 +31,3 @@ def new_list(request) :
         return( render(request, 'home.html', {"error": error}))
     return(redirect('/lists/{}/'.format(list_.id)))
 
-
-def add_item(request, list_id) :
- #   entering_info('add_item', args=[request, list_id])
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list = list_)
-    return(redirect('/lists/{}/'.format(list_id)))
